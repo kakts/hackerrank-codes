703b46f6564e97f45d8641dd982d971c51ed786c
kakts <akutsukeita@gmail.com>
2016-11-20 21:09:41 +0900

    WIP bigger-is-greater

--- implements/bigger-is-greater.py
+++ implements/bigger-is-greater.py
@@ -19,32 +19,39 @@ for i in range(t):
     if aStr.count(firstChar) == strLen:
         answerList.append("no answer")
         continue
-    positionCount = strLen - 2
 
-    changeable = False
-    while positionCount >= 0:
-        if strAsList[positionCount] < strAsList[positionCount + 1]:
-            tmp = strAsList[positionCount]
-            strAsList[positionCount] = strAsList[positionCount + 1]
-            strAsList[positionCount + 1] = tmp
-            changeable = True
+    # Find the pivotable position
+    pivotPos = None
+    for j in range(strLen):
+        if j < strLen - 1 and strAsList[j] < strAsList[j + 1]:
+            pivotPos = j
             break
-        positionCount -= 1
 
-    frontStr = []
-    afterStr = []
-    for k in range(strLen):
-        if k <= positionCount:
-            frontStr.append(strAsList[k])
-        else:
-            afterStr.append(strAsList[k])
-    afterStr.sort()
-
-    replacedStr = frontStr + afterStr
-    print("--", replacedStr, positionCount)
-    if changeable == False:
+    if pivotPos == None:
         answerList.append("no answer")
-    else:
-        answerList.append("".join(replacedStr))
+        continue
+
+    # Find the suffix pivotable position
+    pivotedPos = None
+    for k in range(pivotPos, strLen):
+        if pivotPos < k and strAsList[k] > strAsList[pivotPos]:
+            pivotedPos = k
+
+    tmp = strAsList[pivotPos]
+    strAsList[pivotPos] = strAsList[pivotedPos]
+    strAsList[pivotedPos] = tmp
+
+    prefixList = []
+    suffixList = []
+    for l in range(strLen):
+        data = strAsList[l]
+        if l <= pivotPos:
+            prefixList.append(data)
+        else:
+            suffixList.append(data)
+
+    suffixList.sort()
+    answerList.append("".join(prefixList + suffixList))
+
 for data in answerList:
     print(data)
