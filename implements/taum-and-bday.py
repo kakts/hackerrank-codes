ba1e29514262848ea37f34f086653a8b07c1f4fc
kakts <akutsukeita@gmail.com>
2016-11-25 23:52:07 +0900

    Refactor taum-and-bday

--- implements/taum-and-bday.py
+++ implements/taum-and-bday.py
@@ -14,39 +14,16 @@ for a0 in range(t):
     x,y,z = [int(x),int(y),int(z)]
 
     count = 0
-    if b * x > b * (y + z):
+    if x > y + z:
         count += b * (y + z)
     else:
         count += b * x
 
-    if w * y > w * (x + z):
+    if y > x + z:
         count += w * (x + z)
     else:
         count += w * y
     answerList.append(count)
-"""
-    if b == w:
-        if (2 * x + z) < y:
-            answerList.append(b * (2 * x + z))
-        elif x > (2 * y + z):
-            answerList.append(w * (2 * y + z))
-        else:
-            answerList.append(b * x + w * y)
-        continue
-
-    if b > w:
-        if b * x > w * (x + z):
-            # if exchanging items could be cheaper
-            answerList.append(w * (y + y + z))
-        else:
-            answerList.append(b * x + w * y)
-    else:
-        if w * y > b * (y + z):
-            # if exchanging items could be cheaper
-            answerList.append(b * (x + x + z))
-        else:
-            answerList.append(b * x + w * y)
-"""
 
 for data in answerList:
     print(data)
